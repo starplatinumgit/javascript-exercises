d426647a7941d869c4f56b35a3325cbfcf886450
swfoundation <266610886+starplatinumgit@users.noreply.github.com>
2026-06-15 12:24:39 +1000

    pass all test js 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        for (let i = 0; i < num; i++) {
+            result = result.concat(string);
+            console.log(result);
+        }
+        return result;
+    }
 };
+
+console.log(repeatString("HEY", 5));
+console.log("TE2ST");
 
 // Do not edit below this line
 module.exports = repeatString;
