7c13499ab86bbad672218b6951bd880646568102
swfoundation <266610886+starplatinumgit@users.noreply.github.com>
2026-06-15 12:33:20 +1000

    pass all tests 07 js:

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    return string.split('').reverse().join('');
 
 };
+
+console.log(reverseString("LASREVER"));
 
 // Do not edit below this line
 module.exports = reverseString;
