5462f937fd02f348842bc28b835070f92b61cdb2
swfoundation <266610886+starplatinumgit@users.noreply.github.com>
2026-06-16 07:38:39 +1000

    pass all tests js 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return roundTo((f-32)/1.8);
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return roundTo((c * (9/5)) + 32);
 };
+
+const roundTo = function(num) {
+  return (Math.round(num * 10) / 10);
+}
 
 // Do not edit below this line
 module.exports = {
