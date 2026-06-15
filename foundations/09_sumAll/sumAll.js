2934e9d436e875e844d88f0e2b21fc912de52c2c
swfoundation <266610886+starplatinumgit@users.noreply.github.com>
2026-06-15 16:12:35 +1000

    pass all cases on js 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,48 @@
-const sumAll = function() {
 
-};
+//this code has a bit less redundance... still, the solution just used three if statements 
+//while i may have overcomplicated it using an array.
+//in order to reduce the if statement clutter, i made the numbers into an array 
+//which made the overall code more convoluted (the for loop may be confusing)
+//so its important to look big picture.. a small improvement might cost a big one 
 
+const sumAll = function(firstNum, lastNum) {
+
+    let result = [firstNum, lastNum].sort((num1, num2) => (num1 - num2));   
+    if (result.some((num) => (!(Number.isInteger(num))) || num < 0)) {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    const ARR_MAX = result[1];
+    for (let i = result[0]; i <= ARR_MAX; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+    console.log(sumAll(1, 20));
+
+
+// This is my initial passing code but its pretty ugly:
+
+// const sumAll = function(firstNum, lastNum) {
+
+//     let result = [firstNum, lastNum].sort((num1, num2) => (num1 - num2));   
+//     if (result.some((num) => (!(Number.isInteger(num))) || num < 0)) {
+//         return "ERROR";
+//     }
+//     const ARR_SIZE = result[1] - result[0];
+    
+//     for (let i = 1;  i < ARR_SIZE; i++) {
+//         result.splice(i, 0, 1+result[i-1]);
+//         console.log(result);
+//     }
+
+//     return result.reduce((sum, current) => (sum += current));
+// };
+
+
+
+    
 // Do not edit below this line
 module.exports = sumAll;
